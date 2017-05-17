25e2a7ed9403fc817fd4bdce8c7a0d8057e5127e
michalgol <m1k3m4n@gmail.com>
2017-05-17 09:53:45 +0200

    Dodano pasek wyboru planu

--- src/containers/DiscussionView/index.js
+++ src/containers/DiscussionView/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Divider from 'material-ui/Divider';
 import GridListExampleSingleLine from '../../components/Groups';
 import GridListExampleComplex from '../../components/Classes';
 import CardExampleWithAvatar from '../../components/SpecialEventCard';
@@ -6,7 +7,7 @@ import CardExampleWithAvatar from '../../components/SpecialEventCard';
 class DiscussionView extends React.Component {
   render() {
     return (
-      <div style={{position: 'relative', top: '64px'}}>
+      <div style={{maxWidth: '100%', overflowX: 'hidden', position: 'relative', top: '64px', marginBottom: '56px'}}>
         <CardExampleWithAvatar/>
         <GridListExampleSingleLine />
         <GridListExampleComplex />
