1365be3f4f6e1ec0352fb84d51387f29369c0c77
Jakub Pawlica <jakub.pawlica@gmail.com>
2017-05-20 00:55:56 +0200

    updated streams view

--- src/containers/StreamView/index.js
+++ src/containers/StreamView/index.js
@@ -1,10 +1,58 @@
 import React from 'react'
-import GridListExampleComplex from '../../components/Streams';
+
+import Wrapper from './Wrapper';
+import { GridList } from 'material-ui/GridList';
+import { GridTile } from 'material-ui/GridList';
+
+const styles = {
+  gridList: {
+    width: '100%',
+  },
+  tileStyle: {
+    borderRadius: '3px'
+  },
+  title: {
+    fontSize: "20px"
+  }
+};
+
+const StreamsAPI = {
+  streams: [
+    { number: 1, title: "Pierwszys Stream", image: "http://lorempixel.com/640/640/abstract/2/", featured: true, stories: 21 },
+    { number: 2, title: "Drugi Stream", image: "http://lorempixel.com/640/640/abstract/3/", featured: false, stories: 21 },
+    { number: 3, title: "Trzeci Stream", image: "http://lorempixel.com/640/640/abstract/4/", featured: false, stories: 21 },
+    { number: 4, title: "Czwarty Stream", image: "http://lorempixel.com/640/640/abstract/5/", featured: true, stories: 21 }
+  ],
+  getStreams: function () { return this.streams }
+};
 
 class StreamsView extends React.Component {
   render() {
     return (
-      <GridListExampleComplex />
+      <Wrapper>
+        <GridList
+          cols={2}
+          cellHeight={216}
+          padding={8}
+          style={styles.gridList}
+        >
+          {
+            StreamsAPI.getStreams().map(stream => (
+              <GridTile
+                key={stream.number}
+                style={styles.tileStyle}
+                title={stream.title}
+                titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                titlePosition="bottom"
+                titleStyle={styles.title}
+                cols={stream.featured ? 2 : 1}
+                rows={1}>
+                <img src={stream.image} />
+              </GridTile>
+            ))
+          }
+        </GridList>
+      </Wrapper>
     );
   }
 }
