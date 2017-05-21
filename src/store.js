6cc99b372c08a05922e2d65048a967c404b487f1
Jakub Pawlica <jakub.pawlica@gmail.com>
2017-05-21 11:01:00 +0200

    updated store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const configureStore = () => {
-    const store = createStore(reducers, {})
+    const store = {
+        ...createStore(reducers, composeEnhancers(
+            applyMiddleware()))
+    };
+
     return store;
 };
 
